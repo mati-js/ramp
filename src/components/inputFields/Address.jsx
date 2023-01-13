06bbbbde396fa38a56e91d33bc64f74081d91337
unknown <miribarren@beskar.net>
2023-01-13 15:05:46 -0300

    Subo instancias web3 segun la cripto elegida

--- src/components/inputFields/Address.jsx
+++ src/components/inputFields/Address.jsx
@@ -1,20 +1,43 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import web3Instances from '../shared/Web3';
 import '98.css';
 
 const AddressField = ({selectedCrypto}) => {
   
-  let [addressInput, setAddressInput] = useState('0x');
+  let [addressInput, setAddressInput] = useState('0x')
+    , [addressInfo, setAddressInfo] = useState('');
 
+  useEffect(() => {
+    isValidAddress(addressInput);
+  });
 
-  const isValidAddress = (event) => {
-    setAddressInput(event.target.value);
+  const isValidAddress = async (event) => {
+    try {
+      console.log('Ejecucion');
+      // Get the web3 instance for the selected network and correct the address
+      let web3Instance = web3Instances[selectedCrypto.shortNetworkName];
+      let address = web3Instance.utils.toChecksumAddress(event.target ? event.target.value : event);
+      setAddressInput(address);
+      
+      // Fetch the balance
+      let balance = await web3Instance.eth.getBalance(address);
+      setAddressInfo(`Balance: ${web3Instance.utils.fromWei(balance, 'ether').toString().substring(0, 4)} ${selectedCrypto.symbol}`)
+
+    } catch (error) {
+      // Set the change and set 
+      setAddressInput(event.target ? event.target.value : event);
+      setAddressInfo('Invalid address');
+    }
   }
   
   return (
     <div className='address-container'>
       <fieldset>
         <legend>Paste the address in {selectedCrypto.network}:</legend>
-        <input className='address-input' type='text' value={addressInput} onChange={isValidAddress} />
+        <div className='address-checker'>
+          <input className='address-input' type='text' value={addressInput} onChange={isValidAddress} />
+          <label>{addressInfo}</label>
+        </div>
       </fieldset>
     </div>
   );
---
+++ src/services/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDPsM0KUDtwDbfewC0ZhyGV7n0hrZxqooE",
+  authDomain: "ramp-bfc6c.firebaseapp.com",
+  projectId: "ramp-bfc6c",
+  storageBucket: "ramp-bfc6c.appspot.com",
+  messagingSenderId: "462246848245",
+  appId: "1:462246848245:web:cef3efe436fc56b1b247bc",
+  measurementId: "G-K26QCRC0ET"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
